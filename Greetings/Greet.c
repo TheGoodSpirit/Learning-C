4836e14f51d3fa49f2cbac6b00a34b832cfac4e6
Manish Ghimire <137639133+TheGoodSpirit@users.noreply.github.com>
2023-06-24 22:05:02 +0545

    Take a look

---
+++ Greetings/Greet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	char name[20];
+	
+	printf("Enter your name : ");
+	scanf("%s", name);
+	
+	printf("\nHello, %s", name);
+	
+	return 0;
+	
+}
